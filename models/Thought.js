dbd62e6ed5a84a057804eea466f1ee1452b21904
Heidrun Kubiessa <heidrunkubiessa@heidruns-air.lan>
2023-01-31 15:20:44 -0700

    set up mongoose models

---
+++ models/Thought.js
@@ -0,0 +1,75 @@
+const { Schema, model } = require("mongoose");
+const moment = require("moment");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // mongoose getter method for timestamp
+      get: (timeCreated) =>
+        moment(timeCreated).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Schema to create Post model
+const thoughtSchema = new Schema(
+  {
+    text: {
+      String,
+      required: true,
+      maxLength: 280,
+    },
+
+    username: {
+      String,
+      required: true,
+    },
+
+    reactions: [reactionSchema],
+
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timeCreated) =>
+        moment(timeCreated).format("MMM DD, YYYY [at] hh:mm a"),
+    },
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `commentCount` that gets the amount of comments per post
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+// Initialize our Post model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,58 @@
+const { Schema, model } = require("mongoose");
+
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      String,
+      required: true,
+      unique: true,
+      max_length: 50,
+      trimmed: true,
+    },
+    email: {
+      String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+      required: [true, "User email required"],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+
+  {
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Create a virtual property `fullName` that gets and sets the user's full name
+userSchema.virtual("friendsCount", {
+  ref: "User",
+  localField: "_id",
+  foreignField: "userId",
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
